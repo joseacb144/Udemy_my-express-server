a1a5bdbca842a084257f387aa6e2859cce15398b
Jose Chavez <joseacb144@gmail.com>
2020-06-10 02:33:40 -0500

    Add a 'about' route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get("/contact", function(req, res){
   res.send("<h2>Contact me at: joseacb144@gmail.com</h2>");
 })
 
+app.get("/about", function(req, res){
+  res.send("Hello there! My name is Jose Chavez. I am a computer science student at Texas State University. I should have my Master of Science in Computer Science by Fall 2021!");
+})
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
