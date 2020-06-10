4dffc81381442fd7abc302006427d7d5509407b5
Jose Chavez <joseacb144@gmail.com>
2020-06-10 02:29:04 -0500

    Add another route (contact) for practice.

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(request, response){
-  response.send("<h1>Hello World</h1>");
+app.get("/", function(req, res){
+  res.send("<h1>Hello World</h1>");
+})
+
+app.get("/contact", function(req, res){
+  res.send("<h2>Contact me at: joseacb144@gmail.com</h2>");
 })
 
 app.listen(3000, function(){
